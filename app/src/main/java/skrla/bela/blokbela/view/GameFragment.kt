d8f2291a42cde09c5554fa897841b64e78625006
Matija Skrletović <skrlama@gmail.com>
2022-06-22 23:40:23 +0200

    Added autocoplete editText and validation

--- app/src/main/java/skrla/bela/blokbela/view/GameFragment.kt
+++ app/src/main/java/skrla/bela/blokbela/view/GameFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.activityViewModels
+import com.google.android.material.switchmaterial.SwitchMaterial
+import com.google.android.material.textfield.TextInputEditText
 import skrla.bela.blokbela.data.model.Round
 import skrla.bela.blokbela.databinding.FragmentGameBinding
 import skrla.bela.blokbela.viewmodel.ScoreViewModel
@@ -21,11 +25,38 @@ class GameFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentGameBinding.inflate(inflater, container, false)
+        scoreViewModel.round.observe(viewLifecycleOwner) {
+            if (it == null) {
+                val r = Round(1)
+                scoreViewModel.addRound(r)
+            }
+        }
         val trumpThey = binding.switchTrumpThey
         val trumpUs = binding.switchTrumpUs
         val belaUs = binding.switchBelaUs
         val belaThey = binding.switchBelaThey
         val btn = binding.btnAdd
+        val pointUs = binding.editTextPointUs
+        val pointThem = binding.editTextPointThey
+
+        pointUs.doAfterTextChanged { it ->
+            if(!it.toString().equals("") && pointUs.hasFocus()) {
+                pointThem.setText((162 - it.toString().toInt()).toString())
+            }
+            if(it.toString().equals("") && pointUs.hasFocus()) {
+                pointThem.setText("162")
+            }
+        }
+
+        pointThem.doAfterTextChanged { it ->
+            if(!it.toString().equals("") && pointThem.hasFocus()) {
+                pointUs.setText((162 - it.toString().toInt()).toString())
+            }
+            if(it.toString().equals("") && pointThem.hasFocus()) {
+                pointUs.setText("162")
+            }
+        }
+
         trumpThey.setOnClickListener {
             if (trumpThey.isPressed && trumpUs.isChecked) {
                 trumpUs.isChecked = false
@@ -52,13 +83,53 @@ class GameFragment : Fragment() {
             }
         }
         btn.setOnClickListener {
-            val round = Round( 0)
-            scoreViewModel.insertRound(round)
+            validate(pointUs, pointThem, trumpUs, belaUs, belaThey)
+            setValues(pointUs, pointThem, belaUs, belaThey)
         }
-        
+
         return binding.root
     }
 
+
+    private fun validate(
+        pointUs: TextInputEditText,
+        pointThem: TextInputEditText,
+        trumpUs: SwitchMaterial,
+        belaUs: SwitchMaterial,
+        belaThey: SwitchMaterial
+    ) {
+        val numPointUs = if (pointUs.text.toString() != "") pointUs.text.toString() else "0"
+        val numPointThem = if(pointThem.text.toString() != "") pointThem.text.toString() else "0"
+        val callUs = if (binding.editTextCallUs.text.toString() != ""
+        ) binding.editTextCallUs.text.toString() else "0"
+        val callThem = if (binding.editTextCallThey.text.toString() != ""
+        ) binding.editTextCallThey.text.toString() else "0"
+        val mistake = scoreViewModel.validateInputs(
+            numPointUs.toInt(),
+            numPointThem.toInt(),
+            callUs.toInt(),
+            callThem.toInt()
+        )
+        if(!mistake.equals("")) {
+            Toast.makeText(context, mistake, Toast.LENGTH_LONG).show()
+            return
+        }
+        scoreViewModel.insertScore(trumpUs.isChecked, belaUs.isChecked, belaThey.isChecked)
+    }
+
+    private fun setValues(
+        pointUs: TextInputEditText,
+        pointThem: TextInputEditText,
+        belaUs: SwitchMaterial,
+        belaThey: SwitchMaterial
+    ) {
+        pointUs.setText("0")
+        pointThem.setText("0")
+        binding.editTextCallUs.setText("0")
+        binding.editTextCallThey.setText("0")
+        belaUs.isChecked = false
+        belaThey.isChecked = false
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/skrla/bela/blokbela/viewmodel/ScoreViewModel.kt
+++ app/src/main/java/skrla/bela/blokbela/viewmodel/ScoreViewModel.kt
@@ -1,5 +1,6 @@
 package skrla.bela.blokbela.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
@@ -7,18 +8,82 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import skrla.bela.blokbela.data.GameRepository
 import skrla.bela.blokbela.data.model.Round
+import skrla.bela.blokbela.data.model.Score
 import javax.inject.Inject
 
 @HiltViewModel
 class ScoreViewModel @Inject constructor(private val repository: GameRepository): ViewModel() {
 
-    var round = 1
+    var round: LiveData<Round> = repository.getCurrentRound()
     val score = repository.getScore().asLiveData()
     private val gamePoints = 162
     private val possibleCall = listOf(20, 40, 50, 60, 70, 80, 90, 100, 110, 120, 140, 150, 160, 170, 180, 190, 200, 210, 220, 240, 250, 270, 280, 290, 300, 310, 320, 340, 350, 370, 390, 400)
     private var callerMinPoints = 81
+    private var us = 0
+    private var them = 0
+    private var callUs = 0
+    private var callThem = 0
 
-    fun insertRound(round: Round) = viewModelScope.launch {
-        repository.insertRound(round)
+
+    fun addRound(round: Round) {
+        viewModelScope.launch {
+            repository.insertRound(round)
+        }
+    }
+
+    fun insertScore(caller: Boolean, belaUs: Boolean, belaThem: Boolean) {
+        if (belaUs) {
+            us += 20
+            callerMinPoints += 10
+        }
+        if (belaThem) {
+            them += 20
+            callerMinPoints += 10
+        }
+        if (caller && us < callerMinPoints) {
+            us = 0
+            them = 162 + callUs + callThem
+        } else if (!caller && them < callerMinPoints) {
+            us = 162 + callUs + callThem
+            them = 0
+        } else {
+            us += callUs
+            them += callThem
+        }
+        callerMinPoints = 81
+        val score = Score(0, us, them, 0, round.value!!.roundId)
+        viewModelScope.launch {
+            repository.insertScore(score)
+        }
+        TODO()
+    }
+
+    fun validateInputs(pointUs: Int, pointsThem: Int, callUs: Int, callThem: Int) : String {
+        if (pointUs !in 0..gamePoints) {
+            return "Bodovi tima MI nisu ispravni"
+        }
+        if(pointsThem !in 0..gamePoints) {
+            return "Bodovi tima VI nisu ispravni"
+        }
+        if(pointUs + pointsThem != 162) {
+            return "Nisu isravno unešeni bodovi"
+        }
+        if(callUs !in possibleCall && callUs != 0) {
+            return "Zvanje tima MI nije ispravno"
+        }
+        if (callThem !in possibleCall && callThem != 0) {
+            return "Zvanje tima VI nije ispravno"
+        }
+        if (callUs in possibleCall && callThem != 0) {
+            return "Ne mogu oba tima imati zvanje"
+        }
+        if(callThem in possibleCall && callUs != 0) {
+            return "Ne mogu oba tima imati zvanje"
+        }
+        us = pointUs
+        them = pointsThem
+        this.callUs = callUs
+        this.callThem = callThem
+        return ""
     }
 }
